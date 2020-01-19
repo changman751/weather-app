e121265da34adf5cd356dbfb19086b351dfc1182
changman751 <57850973+changman751@users.noreply.github.com>
2020-01-19 00:05:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,140 +1,149 @@
-let city=""; 
-let url="";
-let APIkey="";
-let queryurl ="";
-let currenturl = "";
-let citiesDiv = document.getElementById("searched_cities_container");
-//start with empty array
-let cities = []; 
-init(); 
-listClicker(); 
-searchClicker(); 
+var apiKey = "ff8e7426ee7910c7a363cd38cbd9b58f7";
+var arr = [];
 
-//run function to pull saved cities from local storage and fill array with it
-function init(){
-    let saved_cities = JSON.parse(localStorage.getItem("cities"));
+var city = "";
+$("button").on("click", function() {
 
-    if (saved_cities !== null){
-        cities = saved_cities
-    }   
-    
-    renderButtons(); 
-}
+    var caContent = $("#get-weather")
+    var letsGo = caContent.val()
+    city = letsGo
 
-//sets localstorage item to cities array 
-function storeCities(){
-    localStorage.setItem("cities", JSON.stringify(cities)); 
-}
+    var dataURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
+    arr.push(letsGo);
 
-//render buttons for each element in cities array as a search history for user
-function renderButtons(){
-    citiesDiv.innerHTML = ""; 
-    if(cities == null){
-        return;
-    }
-    let unique_cities = [...new Set(cities)];
-    for(let i=0; i < unique_cities.length; i++){
-        let cityName = unique_cities[i]; 
+    var searchDiv = document.querySelector(".search-data");
+    var searchList = document.querySelector("#search-list");
 
-        let buttonEl = document.createElement("button");
-        buttonEl.textContent = cityName; 
-        buttonEl.setAttribute("class", "listbtn"); 
-
-        citiesDiv.appendChild(buttonEl);
-        listClicker();
-      }
-    }
-//on click function for search history buttons
-function listClicker(){
-$(".listbtn").on("click", function(event){
-    console.log("anybody home?")
-    event.preventDefault();
-    console.log("hello?");
-    city = $(this).text().trim();
-    APIcalls(); 
-})
-}
-
-
-
-//on click function for main search bar
-function searchClicker() {
-$("#searchbtn").on("click", function(event){
-    event.preventDefault();
-    city = $(this).prev().val().trim()
-    
-    //push the city user entered into cities array 
-    cities.push(city);
-    //make sure cities array.length is never more than 8 
-    if(cities.length > 8){
-        cities.shift()
-    }
-    //return from function early if form is blank
-    if (city == ""){
-        return; 
-    }
-    APIcalls();
-    storeCities(); 
-    renderButtons();
-})
-}
-
-//runs 2 API calls, one for current weather data and one for five-day forecast, then populates text areas
-function APIcalls(){
-    
-    url = "https://api.openweathermap.org/data/2.5/forecast?q=";    
-    currenturl = "https://api.openweathermap.org/data/2.5/weather?q=";
-    APIkey = "f8e7426ee7910c7a363cd38cbd9b58f7";
-    queryurl = url + city + APIkey;
-    current_weather_url = currenturl + city + APIkey; 
-    
-    $("#name_of_city").text("Today's Weather in " + city);
-    $.ajax({
-        url: queryurl,
-        method: "GET",
-        
-    }).then(function(response){
-        let day_number = 0; 
-        
-        //iterate through the 40 weather data sets
-        for(let i=0; i< response.list.length; i++){
-            
-            //split function to isolate the time from the time/data aspect of weather data, and only select weather reports for 3pm
-            if(response.list[i].dt_txt.split(" ")[1] == "15:00:00")
-            {
-                //if time of report is 3pm, populate text areas accordingly
-                let day = response.list[i].dt_txt.split("-")[2].split(" ")[0];
-                let month = response.list[i].dt_txt.split("-")[1];
-                let year = response.list[i].dt_txt.split("-")[0];
-                $("#" + day_number + "date").text(month + "/" + day + "/" + year); 
-                let temp = Math.round(((response.list[i].main.temp - 273.15) *9/5+32));
-                $("#" + day_number + "five_day_temp").text("Temp: " + temp + String.fromCharCode(176)+"F");
-                $("#" + day_number + "five_day_humidity").text("Humidity: " + response.list[i].main.humidity);
-                $("#" + day_number + "five_day_icon").attr("src", "http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png");
-                console.log(response.list[i].dt_txt.split("-"));
-                console.log(day_number);
-                console.log(response.list[i].main.temp);
-                day_number++; 
-                        }   
+    function renderCities() {
+        searchList.textContent = arr.length;
+        for (var i = 0; i < arr.length; i++) {
+            var results = arr[i];
+            var li = document.createElement("li");
+            li.textContent = results;
+            $(searchList.append(results));
         }
+    }
+    renderCities()
+
+
+    // // var str = JSON.stringify(city);
+    // // var obj = JSON.parse(str)
+    // $(".search-data").append(city + "<br>");
+    // localStorage.setItem("City", arr);
+    // localStorage.getItem("City")
+
+    // Here we run our AJAX call to the OpenWeatherMap API
+    $.ajax({
+        url: dataURL,
+        method: "GET"
+    }).then(function(response) { // We store all of the retrieved data inside of an object called "response"
+        var iconcode = response.weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        // Log the dataURL
+        console.log(dataURL);
+
+        // Log the resulting object
+        console.log(response);
+
+        // Converts the temp to Kelvin with the below formula & then sets it to 2 decimal points
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        var temp = tempF.toFixed(2)
+
+        // Transfer content to HTML
+        $(".city").html("<h2>Today in " + city + "</h2>");
+        $('#wicon').attr('src', iconurl);
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".temp").text("Temperature (F): " + temp);
+
+
+        // Log the data in the console as well
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + response.main.temp);
     });
 
+    // Second AJAX call for 5 day forecast
 
-    //function to display data in main div 
-     $.ajax({
-         url:current_weather_url,
-         method: "GET", 
-     }).then(function(current_data){
-         console.log(current_data);
-         let temp = Math.round(((current_data.main.temp - 273.15) * 9/5 + 32))
-         console.log("The temperature in " + city + " is: " + temp);
-         $("#today_temp").text("Temperature: " + temp + String.fromCharCode(176)+"F");
-         $("#today_humidity").text("Humidity: " + current_data.main.humidity);
-         $("#today_wind_speed").text("Wind Speed: " + current_data.wind.speed);
-         $("#today_icon_div").attr({"src": "http://openweathermap.org/img/w/" + current_data.weather[0].icon + ".png",
-          "height": "100px", "width":"100px"});
-     })
-    
+    var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
-}
+    $.ajax({
+        url: fiveDayURL,
+        method: "GET"
+    }).then(function(responseTwo) {
+        // Log the queryURL
+        console.log(fiveDayURL);
+
+        // Log the resulting object
+        console.log(responseTwo);
+
+        // Add the icon based on the forecasted weather
+        // Day 1
+        var icon1 = responseTwo.list[4].weather[0].icon;
+        var icon1url = "http://openweathermap.org/img/w/" + icon1 + ".png";
+        // Day 2
+        var icon2 = responseTwo.list[12].weather[0].icon;
+        var icon2url = "http://openweathermap.org/img/w/" + icon2 + ".png";
+        // Day 3
+        var icon3 = responseTwo.list[20].weather[0].icon;
+        var icon3url = "http://openweathermap.org/img/w/" + icon3 + ".png";
+        // Day 4
+        var icon4 = responseTwo.list[28].weather[0].icon;
+        var icon4url = "http://openweathermap.org/img/w/" + icon4 + ".png";
+        // Day 5
+        var icon5 = responseTwo.list[36].weather[0].icon;
+        var icon5url = "http://openweathermap.org/img/w/" + icon5 + ".png";
+
+        // Converts the temp to Kelvin with the below formula & then sets it to 2 decimal points
+        var tempOneF = (responseTwo.list[4].main.temp - 273.15) * 1.80 + 32;
+        var tempOne = tempOneF.toFixed(2);
+
+        var tempTwoF = (responseTwo.list[12].main.temp - 273.15) * 1.80 + 32;
+        var tempTwo = tempTwoF.toFixed(2);
+
+        var tempThreeF = (responseTwo.list[20].main.temp - 273.15) * 1.80 + 32;
+        var tempThree = tempThreeF.toFixed(2);
+
+        var tempFourF = (responseTwo.list[28].main.temp - 273.15) * 1.80 + 32;
+        var tempFour = tempFourF.toFixed(2);
+
+        var tempFiveF = (responseTwo.list[36].main.temp - 273.15) * 1.80 + 32;
+        var tempFive = tempFiveF.toFixed(2);
+
+        // attempting to use substr() to only show the date and remove the hours
+        var dayone = responseTwo.list[4].dt_txt;
+        var daytwo = responseTwo.list[12].dt_txt;
+        var daythree = responseTwo.list[20].dt_txt;
+        var dayfour = responseTwo.list[28].dt_txt;
+        var dayfive = responseTwo.list[36].dt_txt;
+
+
+        $("#day-1").html("<h6>" + dayone.substr(0, 10) + "</h6>")
+        $("#day-1").append("<img src=" + icon1url + ">")
+        $("#day-1").append("<p>" + "Temp: " + tempOne + "</p>")
+        $("#day-1").append("<p>" + "Humidity: " + responseTwo.list[4].main.humidity + "</p>")
+
+        $("#day-2").html("<h6>" + daytwo.substr(0, 10) + "</h6>")
+        $("#day-2").append("<img src=" + icon2url + ">")
+        $("#day-2").append("<p>" + "Temp: " + tempTwo + "</p>")
+        $("#day-2").append("<p>" + "Humidity: " + responseTwo.list[12].main.humidity + "</p>")
+
+        $("#day-3").html("<h6>" + daythree.substr(0, 10) + "</h6>")
+        $("#day-3").append("<img src=" + icon3url + ">")
+        $("#day-3").append("<p>" + "Temp: " + tempThree + "</p>")
+        $("#day-3").append("<p>" + "Humidity: " + responseTwo.list[20].main.humidity + "</p>")
+
+        $("#day-4").html("<h6>" + dayfour.substr(0, 10) + "</h6>")
+        $("#day-4").append("<img src=" + icon4url + ">")
+        $("#day-4").append("<p>" + "Temp: " + tempFour + "</p>")
+        $("#day-4").append("<p>" + "Humidity: " + responseTwo.list[28].main.humidity + "</p>")
+
+        $("#day-5").html("<h6>" + dayfive.substr(0, 10) + "</h6>")
+        $("#day-5").append("<img src=" + icon5url + ">")
+        $("#day-5").append("<p>" + "Temp: " + tempFive + "</p>")
+        $("#day-5").append("<p>" + "Humidity: " + responseTwo.list[36].main.humidity + "</p>")
+
+    });
+
+})
